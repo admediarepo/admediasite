ee0992e431d9ebf22b6fc1366553616ab49b92d6
Eduardo Gouveia <qwerty124563@gmail.com>
2017-05-22 15:34:54 +0100

    Update db.sql

--- sql/1.sql
+++ sql/1.sql
@@ -15,6 +15,8 @@
 -- Data exporting was unselected.
 -- Data exporting was unselected.
 -- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
