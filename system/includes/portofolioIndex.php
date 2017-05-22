e97990aa00f7d16a2b96e06d1220c4dc78995b70
13dev <qwerty124563@gmail.com>
2017-05-22 20:28:03 +0100

    Update portofolio index and clean files

--- system/includes/portofolioIndex.php
+++ system/includes/portofolioIndex.php
@@ -4,9 +4,14 @@
 
 	<div class="6u 12u(narrower)">
 		<section style="word-wrap: break-word;text-align: justify;">
-			<a href="#<?= $trabalho['id'] ?>" class="image featured"><img src="images/projects/<?= $trabalho['mainimage'] ?>" alt="<?= $trabalho['title'] ?>" width="100" /></a>
+			<a href="#<?= $trabalho['id'] ?>" class="image featured">
+			<?php if(file_exists('images/projects/'.$trabalho['mainimage'])): ?>
+			<img src="images/projects/<?= $trabalho['mainimage'] ?>" alt="<?= $trabalho['title'] ?>" width="100" /></a>
+		<?php else: ?>
+			<img src="images/projects/noimg.png" alt="<?= $trabalho['title'] ?>" width="100" /></a>
+		<?php endif; ?>
 			<header>
-				<h3><?= $trabalho['title'] ?></h3>
+				<h3 style="text-align:center;"><?= $trabalho['title'] ?></h3>
 			</header>
 			<p><?= $trabalho['sum'] ?></p>
 		</section>
