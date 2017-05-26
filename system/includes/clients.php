152f65e38405150f8d2e026207a7a278d0fcbdca
Eduardo Gouveia <a13734@ebsaas.com>
2017-05-26 15:42:16 +0100

    add file clients

---
+++ system/includes/clients.php
@@ -0,0 +1,11 @@
+<div class="6u 12u(narrower)">
+	<section>
+		<ul>
+			<li><img src="http://placehold.it/200x200"></li>
+			<li><img src="http://placehold.it/200x200"></li>
+			<li><img src="http://placehold.it/200x200"></li>
+			<li><img src="http://placehold.it/200x200"></li>
+			<li><img src="http://placehold.it/200x200"></li>
+		</ul>
+	</section>
+</div>
