e1cb885ddf140ce22d095e84b9d8129cf9cdc11c
Eduardo Gouveia <a13734@ebsaas.com>
2017-05-30 16:53:24 +0100

    clientes

--- system/includes/clients.php
+++ system/includes/clients.php
@@ -1,24 +1,8 @@
 <div class="12u 12u(narrower)">
-	<div style="display: block; margin: 0 auto;">
-		<div style="display:inline-block;" class="2u 12u(narrower)">
-			<section class="clients">
-				<img src="images/projects/noimg.png" alt="1"/>
-			</section>
-		</div>
-		<div style="display:inline-block;" class="2u 12u(narrower)">
-			<section class="clients">
-				<img src="images/projects/noimg.png" alt="1"/>
-			</section>
-		</div>
-		<div style="display:inline-block;" class="2u 12u(narrower)">
-			<section class="clients">
-				<img src="images/projects/noimg.png" alt="1"/>
-			</section>
-		</div>
-		<div style="display:inline-block;"class="2u 12u(narrower)">
-			<section class="clients">
-				<img src="images/projects/noimg.png" alt="1"/>
-			</section>
-		</div>
-	</div>
+	<section class="clients">
+		<img src="images/projects/noimg.png" alt="1"/>
+		<img src="images/projects/noimg.png" alt="1"/>
+		<img src="images/projects/noimg.png" alt="1"/>
+		<img src="images/projects/noimg.png" alt="1"/>
+	</section>
 </div>
